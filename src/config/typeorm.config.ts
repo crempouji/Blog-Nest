e95ee9b111f8bbbc3849b0b58ae68ec11b85582c
crempouji <gabriel.crempe@usp.br>
2020-04-01 13:03:56 -0300

    DB Config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+      dropSchema: false,
+    };
+  }
+}
